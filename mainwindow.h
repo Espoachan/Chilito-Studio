5b088a0504479be6a9b17b7515199a744ec660c8
Espoachan <leoponce200111@gmail.com>
2024-10-26 15:58:06 -0600

    Corregi un error que no dejaba compilar

--- mainwindow.h
+++ mainwindow.h
@@ -14,11 +14,6 @@
 #include <QSpinBox>
 #include <QMouseEvent>
 #include <QPainter>
-#include <QFileDialog>
-#include <QFile>
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QJsonArray>
 #include "layer.h"
 
 class Canvas : public QGraphicsView {
@@ -35,16 +30,16 @@ public:
 
     void addLayer() {
         Layer *layer = new Layer(size());
-        layer->setZValue(layers.size());
+        layer->setZValue(layers.size());  // Asegura que cada capa esté en una posición única
         scene()->addItem(layer);
         layers.append(layer);
         setCurrentLayer(layer);
     }
 
     void setCurrentLayer(Layer *layer) {
-        if (currentLayer) currentLayer->setVisible(false);
+        if (currentLayer) currentLayer->setVisible(false);  // Oculta la capa anterior
         currentLayer = layer;
-        currentLayer->setVisible(true);
+        currentLayer->setVisible(true);  // Muestra la capa seleccionada
     }
 
     void selectLayer(int index) {
@@ -66,47 +61,6 @@ public:
         eraserMode = true;
     }
 
-    void saveToFile(const QString &filePath) {
-        QJsonArray layerArray;
-        for (Layer *layer : layers) {
-            QJsonObject layerObject;
-            layerObject["content"] = layer->saveLayerContent();  // Asegúrate de que `saveLayerContent()` esté implementado en Layer
-            layerArray.append(layerObject);
-        }
-
-        QJsonObject saveObject;
-        saveObject["layers"] = layerArray;
-
-        QFile file(filePath);
-        if (file.open(QIODevice::WriteOnly)) {
-            file.write(QJsonDocument(saveObject).toJson());
-            file.close();
-        }
-    }
-
-    void loadFromFile(const QString &filePath) {
-        QFile file(filePath);
-        if (file.open(QIODevice::ReadOnly)) {
-            QJsonDocument loadDoc(QJsonDocument::fromJson(file.readAll()));
-            QJsonArray layerArray = loadDoc["layers"].toArray();
-
-            for (Layer *layer : layers) {
-                delete layer;  // Limpia las capas existentes
-            }
-            layers.clear();
-
-            for (const QJsonValue &value : layerArray) {
-                Layer *layer = new Layer(size());
-                layer->loadLayerContent(value.toObject());  // Asegúrate de que `loadLayerContent()` esté implementado en Layer
-                scene()->addItem(layer);
-                layers.append(layer);
-            }
-
-            setCurrentLayer(layers.isEmpty() ? nullptr : layers.last());
-            file.close();
-        }
-    }
-
 protected:
     void mousePressEvent(QMouseEvent *event) override {
         if (currentLayer) {
@@ -118,9 +72,9 @@ protected:
         if (currentLayer && event->buttons() & Qt::LeftButton) {
             QPointF newPoint = mapToScene(event->pos());
             if (eraserMode) {
-                currentLayer->eraseLine(lastPoint, newPoint, pen.width());
+                currentLayer->eraseLine(lastPoint, newPoint, pen.width());  // Modo borrador
             } else {
-                currentLayer->drawLine(lastPoint, newPoint, pen);
+                currentLayer->drawLine(lastPoint, newPoint, pen);  // Modo dibujo
             }
             lastPoint = newPoint;
         }
@@ -131,7 +85,7 @@ private:
     Layer *currentLayer;
     QPointF lastPoint;
     QPen pen;
-    bool eraserMode;
+    bool eraserMode;  // Indica si estamos en modo borrador o no
 };
 
 class MainWindow : public QMainWindow {
@@ -171,15 +125,11 @@ private:
         QAction *penAction = mainToolBar->addAction("Lápiz");
         QAction *eraserAction = mainToolBar->addAction("Borrador");
         QAction *colorAction = mainToolBar->addAction("Color");
-        QAction *saveAction = mainToolBar->addAction("Guardar Proyecto");
-        QAction *loadAction = mainToolBar->addAction("Cargar Proyecto");
 
         connect(newFrameAction, &QAction::triggered, this, &MainWindow::addNewLayer);
         connect(penAction, &QAction::triggered, this, [=]() { canvas->setPenColor(currentColor); });
         connect(eraserAction, &QAction::triggered, this, &MainWindow::activateEraser);
         connect(colorAction, &QAction::triggered, this, &MainWindow::showColorDialog);
-        connect(saveAction, &QAction::triggered, this, &MainWindow::saveProject);
-        connect(loadAction, &QAction::triggered, this, &MainWindow::loadProject);
     }
 
     void createTimelinePanel() {
@@ -269,28 +219,10 @@ private:
     }
 
     void showColorDialog() {
-        QColor color = QColorDialog::getColor(currentColor, this);
+        QColor color = QColorDialog::getColor(currentColor, this, "Selecciona un color");
         if (color.isValid()) {
             currentColor = color;
             canvas->setPenColor(currentColor);
-        }
-    }
-
-    void saveProject() {
-        QString fileName = QFileDialog::getSaveFileName(this, "Guardar Proyecto", "", "JSON Files (*.json)");
-        if (!fileName.isEmpty()) {
-            canvas->saveToFile(fileName);
-        }
-    }
-
-    void loadProject() {
-        QString fileName = QFileDialog::getOpenFileName(this, "Cargar Proyecto", "", "JSON Files (*.json)");
-        if (!fileName.isEmpty()) {
-            canvas->loadFromFile(fileName);
-            layersList->clear();
-            for (int i = 0; i < canvas->layerCount(); ++i) {  // Asegúrate de implementar layerCount() en Canvas
-                layersList->addItem("Layer " + QString::number(i + 1));
-            }
         }
     }
 };
